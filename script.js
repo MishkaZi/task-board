7e802a3249764f1bf219784a1daf69c47ddb230c
Michael Zinoviev <mishazinoviev91@gmail.com>
2020-11-16 14:16:20 +0200

    Html and styles finished

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+const noteData = document.getElementById('note-data-input');
+const noteDate = document.getElementById('note-date-input');
+const noteTime = document.getElementById('note-time-input');
+
+const noteDataPrint = document.getElementById('note-data');
+const noteDatePrint = document.getElementById('note-date');
+const noteTimePrint = document.getElementById('note-time');
+
+
+const NoteDiv = document.getElementById('note-div');
+
+
